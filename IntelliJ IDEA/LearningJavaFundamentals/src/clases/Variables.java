fad9dd74338eb56cfe91d9c7b74040916a0cc0dd
Sher27 <133188059+Sher27@users.noreply.github.com>
2025-01-18 14:27:38 -0600

    primer commit 2

---
+++ IntelliJ IDEA/LearningJavaFundamentals/src/clases/Variables.java
@@ -0,0 +1,22 @@
+package clases;
+
+public class Variables {
+    public static void main(String[] args) {
+        //Variables
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+
+
+        int initValue;
+        initValue = 100;
+        System.out.println(initValue);
+
+
+        long hugeValue = 100000000L;
+        System.out.println(hugeValue);
+
+        
+        float floatVar = 3.14F;
+        System.out.println(floatVar);
+    }
+}
